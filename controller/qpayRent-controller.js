e68aba8419892c5fe9ef5f95c47b72cd9d84e560
n1steka <mongolzkarigun@gmail.com>
2024-03-04 19:20:49 +0800

    3n sar iin draa ustgana

--- controller/qpayRent-controller.js
+++ controller/qpayRent-controller.js
@@ -166,16 +166,16 @@ exports.callback = asyncHandler(async (req, res, next) => {
 
         // 3n sar bolgoj solino 
 
-        let targetDate = new Date();
-        targetDate.setMinutes(targetDate.getMinutes() + 120);
+        //  let targetDate = new Date();
+        //       targetDate.setMinutes(targetDate.getMinutes() + 120);
 
-        const delay = targetDate - Date.now();
+        //       const delay = targetDate - Date.now();
 
         //test  duussaniii daraa ene functioniig ashiglah 
 
-        // let targetDate = new Date();
-        // targetDate.setMonth(targetDate.getMonth() + 3);
-        // const delay = targetDate - Date.now();
+        let targetDate = new Date();
+        targetDate.setMonth(targetDate.getMonth() + 3);
+        const delay = targetDate.getTime() - Date.now();
 
 
 
