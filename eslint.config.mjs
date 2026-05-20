c26f9759a7ba4b7414aa3124cf164a715c81b2e3
Ankhaa <anhaaidiot@gmail.com>
2026-05-21 01:54:06 +0800

    chore: add eslint-plugin-react-hooks for rules-of-hooks checking

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import reactHooks from 'eslint-plugin-react-hooks';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      '@typescript-eslint': typescriptEslint
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    },
+  },
+];
